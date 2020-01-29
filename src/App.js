4fc05d60a57ff04a3c8fc7a8319513db6b329b59
phazel <courtney.p.hazel@gmail.com>
2020-01-29 11:19:47 +1100

    Add rudimentary options with terrible state management

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,9 +16,228 @@ function App() {
         >
           Learn React
         </a>
+
+        <ExampleApp />
       </header>
     </div>
   );
+}
+
+const Checkbox = props => (
+  <input type="checkbox" {...props} />
+)
+
+class ExampleApp extends React.Component {
+  state = { checked: false }
+  handleCheckboxChange = event =>
+  this.setState({ checked: event.target.checked })
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Select Which Apply To You</h2>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Man</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Woman</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Non-Binary</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Transgender</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Agender</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Two-Spirit</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Gender-Non-Conforming</span>
+          </label>
+        </div>
+        <div>
+          <h2>Select Which Apply To You</h2>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Straight</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Gay</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Lesbian</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Bisexual</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Pansexual</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Sapphic</span>
+          </label>
+        </div>
+        <div>
+          <h2>Select Which You're Looking for</h2>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Man</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Woman</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Non-Binary</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Transgender</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Agender</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Two-Spirit</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Gender-Non-Conforming</span>
+          </label>
+        </div>
+        <div>
+          <h2>Select Which You Don't Want To Interact With</h2>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Man</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Woman</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Non-Binary</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Transgender</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Agender</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Two-Spirit</span>
+          </label>
+          <label className="option">
+            <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+            />
+            <span>Gender-Non-Conforming</span>
+          </label>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
